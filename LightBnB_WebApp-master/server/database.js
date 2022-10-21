699c1d4a39c90fe989090656a4d52a4f4557e05a
Yongjia Sun <thomastank0926@gmail.com>
2022-10-21 09:56:16 +0000

    refactor the getAllProperties function to imply filter

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -135,18 +135,56 @@ const getAllProperties = function(options, limit = 10) {
   //   limitedProperties[i] = properties[i];
   // }
   // return Promise.resolve(limitedProperties);
-  return pool
-    .query(
-      `SELECT *
-      FROM properties
-      LIMIT $1`,
-      [ limit ])
-    .then((result) => {
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+ // 1
+ const queryParams = [];
+ // 2
+ let queryString = `
+ SELECT properties.*, avg(property_reviews.rating) as average_rating
+ FROM properties
+ JOIN property_reviews ON properties.id = property_id
+ `;
+
+ // 3
+ if (options.city) {
+   queryParams.push(`%${options.city}%`);
+   queryString += `WHERE city LIKE $${queryParams.length} `;
+ }
+
+ if (options.minimum_price_per_night && options.maximum_price_per_night) {
+  queryParams.push(options.minimum_price_per_night, options.maximum_price_per_night);
+  queryString += `AND cost_per_night / 100 <= $${queryParams.length} AND cost_per_night / 100 >= $${queryParams.length - 1}`;
+}
+
+ if (options.owner_id) {
+  queryParams.push(options.owner_id);
+  queryString += ` WHERE owner_id = $${queryParams.length} `;
+}
+
+ // 4
+ queryString += `
+ GROUP BY properties.id
+ `;
+
+ if (options.minimum_rating) {
+  queryParams.push(Number(options.minimum_rating));
+  queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length}`;
+}
+
+ queryParams.push(limit);
+ queryString += `
+ ORDER BY cost_per_night
+ LIMIT $${queryParams.length};
+ `;
+
+ // 5
+ console.log(queryString, queryParams);
+
+ // 6
+ return pool.query(queryString, queryParams)
+  .then((res) => res.rows)
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 exports.getAllProperties = getAllProperties;
 
